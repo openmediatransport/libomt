9a0724cbb24d133a743f16c17acb73a3e2467447
omtmartin <martin@openmediatransport.org>
2025-07-24 21:04:22 +1000

    Mac Universal build script added

--- build/buildmacarm64.sh
+++
@@ -1,3 +0,0 @@
-dotnet publish ../libomt.sln -r osx-arm64 -c Release
-install_name_tool -id libomt.dylib ../bin/Release/net8.0/osx-arm64/publish/libomt.dylib
-install_name_tool -id libomt.dylib ../bin/Release/net8.0/osx-arm64/native/libomt.dylib
--- build/buildmacx64.sh
+++
@@ -1,3 +0,0 @@
-dotnet publish ../libomt.sln -r osx-x64 -c Release
-install_name_tool -id libomt.dylib ../bin/Release/net8.0/osx-x64/publish/libomt.dylib
-install_name_tool -id libomt.dylib ../bin/Release/net8.0/osx-x64/native/libomt.dylib
